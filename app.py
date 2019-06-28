d42edf591ff193f129ee21158d6e256ab80407c7
DurlaX88 <46277593+DurlaX88@users.noreply.github.com>
2019-06-28 18:01:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
-import time
-import random
-from discord import Game
-
 
 Client = discord.client
 client = commands.Bot(command_prefix = '!')
